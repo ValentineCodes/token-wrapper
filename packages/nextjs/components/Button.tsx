32e54dbf092134306f7fc45cedcdaa39fe0c9b7e
ValentineCodes <Valentineorga@gmail.com>
2023-05-31 22:46:34 +0100

    added outline style to custom Button

--- packages/nextjs/components/Button.tsx
+++ packages/nextjs/components/Button.tsx
@@ -5,12 +5,13 @@ type Props = {
     label: string;
     className?: string;
     isLoading?: boolean;
+    outline?: boolean;
     onClick: () => void
 }
 
-function Button({label, className, isLoading, onClick}: Props) {
+function Button({label, className, isLoading, outline, onClick}: Props) {
   return (
-    <button className={`border border-[#624DE3] bg-[#624DE3] text-white hover:bg-white hover:text-[#624DE3] transition-all px-4 py-2 rounded-md shadow-lg w-full mt-5 ${className}`} onClick={onClick}>{isLoading ? <Spinner size="md" thickness='4px' speed='0.65s' /> : label}</button>
+    <button className={`border border-[#624DE3] ${outline? ' bg-white text-[#624DE3] hover:bg-[#624DE3] hover:text-white' : 'bg-[#624DE3] text-white hover:bg-white hover:text-[#624DE3]'} transition-all px-4 py-2 rounded-md shadow-lg w-full mt-5 ${className}`} onClick={onClick}>{isLoading ? <Spinner size="md" thickness='4px' speed='0.65s' /> : label}</button>
   )
 }
 
