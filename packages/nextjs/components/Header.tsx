53af66424a622f085ddbccb1f340ae1db146f7ca
ValentineCodes <Valentineorga@gmail.com>
2023-06-01 15:34:36 +0100

    remove navlinks

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -87,7 +87,7 @@ export const Header = () => {
             <span className="text-xs">Wrap and Bridge Tokens</span>
           </div>
         </Link>
-        <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">{navLinks}</ul>
+        {/* <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">{navLinks}</ul> */}
       </div>
       <div className="navbar-end flex-grow mr-4">
         <RainbowKitCustomConnectButton />
